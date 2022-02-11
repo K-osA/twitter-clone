7fa04f81bf7efa1587babdcc3dffb50f01a92684
K-osA <evbayooooo@gmail.com>
2022-02-11 21:47:30 +0900

    ordered

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,13 +5,18 @@ import React, { useEffect, useState } from "react";
 const Home = ({ userObj }) => {
   const [tweets, setTweets] = useState([]);
   useEffect(() => {
-    dbService.collection("tweets").onSnapshot((snapshot) => {
-      const tweetArray = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setTweets(tweetArray);
-    });
+    dbService
+      .collection("tweets")
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snapshot) => {
+        const tweetArray = snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        setTweets(tweetArray);
+      });
   }, []);
 
   return (
